e497eb79307ea5e99a40b4846c00dd83b0f45914
Evgenii Popov <webviser@mail.ru>
2024-12-04 01:55:58 +0600

    wip

--- project/libs/account/blog-user/src/blog-user-module/blog-user.module.ts
+++ project/libs/account/blog-user/src/blog-user-module/blog-user.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
 
-@Module({})
+import { BlogUserRepository } from './blog-user.repository';
+import { BlogUserFactory } from './blog-user.factory';
+import { BlogUserController } from './blog-user.controller';
+
+@Module({
+  providers: [BlogUserRepository, BlogUserFactory],
+  controllers: [BlogUserController],
+  exports: [BlogUserRepository],
+})
 export class BlogUserModule {}
