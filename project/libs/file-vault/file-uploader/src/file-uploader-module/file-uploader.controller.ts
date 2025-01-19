a7e75c35c82e9a5da08f81edc35141ba6842791b
Evgenii Popov <webviser@mail.ru>
2025-01-20 00:34:48 +0600

    port 6000 used for x11 @ApiConsumes work with @Body

--- project/libs/file-vault/file-uploader/src/file-uploader-module/file-uploader.controller.ts
+++ project/libs/file-vault/file-uploader/src/file-uploader-module/file-uploader.controller.ts
@@ -1,9 +1,9 @@
-import 'multer';
 import { Express } from 'express';
 import {
   Controller,
   Get,
   HttpStatus,
+  Inject,
   Param,
   Post,
   SerializeOptions,
@@ -14,14 +14,16 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { FileUploaderService } from './file-uploader.service';
 import { MongoIdValidationPipe } from '@project/pipes';
 import { UploadedFileRdo } from './rdo/uploaded-file.rdo';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiConsumes, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { SWAGGER_RESPONSE, SWAGGER_SUMMARY } from './file-uploader.constant';
 
 @Controller('files')
-@ApiTags('file-vault')
+@ApiTags('Works with files')
 @SerializeOptions({ type: UploadedFileRdo, excludeExtraneousValues: true })
 export class FileUploaderController {
-  constructor(private readonly fileUploaderService: FileUploaderService) {}
+  constructor(
+    @Inject(FileUploaderService) private readonly fileUploaderService: FileUploaderService
+  ) {}
 
   @Post('/upload')
   @ApiOperation({ summary: SWAGGER_SUMMARY.UPLOAD })
@@ -31,17 +33,28 @@ export class FileUploaderController {
     description: SWAGGER_RESPONSE.CREATED,
   })
   @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: SWAGGER_RESPONSE.BAD })
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    required: true,
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+      required: ['file'],
+    },
+  })
   @UseInterceptors(FileInterceptor('file'))
   public async uploadFile(@UploadedFile() file: Express.Multer.File) {
     return this.fileUploaderService.saveFile(file);
   }
 
   @Get(':fileId')
-  @ApiResponse({
-    status: HttpStatus.OK,
-    type: UploadedFileRdo,
-    description: SWAGGER_RESPONSE.OK,
-  })
+  @ApiResponse({ status: HttpStatus.OK, type: UploadedFileRdo, description: SWAGGER_RESPONSE.OK })
+  @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: SWAGGER_RESPONSE.BAD })
   @ApiResponse({ status: HttpStatus.NOT_FOUND, description: SWAGGER_RESPONSE.NOT_FOUND })
   public async show(@Param('fileId', MongoIdValidationPipe) fileId: string) {
     return this.fileUploaderService.getFile(fileId);
--- project/libs/file-vault/file-uploader/src/file-uploader-module/rdo/uploaded-file.rdo.ts
+++ project/libs/file-vault/file-uploader/src/file-uploader-module/rdo/uploaded-file.rdo.ts
@@ -1,21 +1,28 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Expose } from 'class-transformer';
 
 export class UploadedFileRdo {
   @Expose()
+  @ApiProperty()
   public id: string;
 
   @Expose()
+  @ApiProperty()
   public originalName: string;
 
   @Expose()
+  @ApiProperty()
   public hashName: string;
 
   @Expose()
+  @ApiProperty()
   public subDirectory: string;
 
   @Expose()
+  @ApiProperty()
   public mimetype: string;
 
   @Expose()
+  @ApiProperty()
   public size: number;
 }
